453725e57b2c2316ea368688f92c3031c138621a
Gabriel Almeida <gabrielalmeidads@gmail.com>
2019-04-07 01:51:56 -0300

    primeiro commit

---
+++ src/jogo/BancoPalavras.java
@@ -0,0 +1,22 @@
+package jogo;
+public class BancoPalavras /*classe destinada ao armazenamento das palavras e das dicas*/ {
+	public static String [] palavras ={"CAFE","HOMEM","MULHER","MENINIO","MENINA",
+			"EU","VOCE","ELE","ELA","CEU","VIOLAO","POSSIVEL","ONIBUS","VEICULO",
+			"CASA","TREM","AVIAO","PALAVRA","VIDA","MUSICA","RELOGIO","TEXTO","TELEVISAO",
+			"CACHORRO","GATO","MACACO","CHAVE","ESTADO","PAIS","AGUA","SUCO","REFRIGERANTE",
+			"COMIDA","CELULAR","PORCO","DENTE","BOCA","ESCOLA","COMPUTADOR","PIANO"};
+	//vetor destinada ao armazenamento das palavras
+	
+	
+	
+	public static String [] dicas = {"BEBIDA", "PESSOA", "PESSOA", "CRIANCA", "CRIANCA",
+			"INDICATIVO", "INDICATIVO", "INDICATIVO", "INDIVATIVO", "AZUL", "CORDAS",
+			"PROBABILIDADE", "VEICULO", "CARRO", "TIJOLOS", "LOCOMOCAO", "CEU", "PORTUGUES",
+			"TODOS TEM", "SOM", "HORAS", "ESCREVER", "NOVELA", "ANIMAL", "ANIMAL",
+			"USA NO CARRO", "PORTA", "POSSUI CIDADE", "LOCAL", "MOLHADA", "LIQUIDO",
+			"LIQUIDO GASOSO", "ALIMENTO", "ELETRONICO", "SUJO", "BOCA", "DENTISTA", "ESTUDOS",
+			"INFORMATICA", "INSTRUMENTO"};
+	//vetor destinado ao armazenamento das dicas
+	
+}
+	
