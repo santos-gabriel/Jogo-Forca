a8995476c1a378de7ad50bfecc49cc98f2fe3e6e
Gabriel Almeida <gabrielalmeidads@gmail.com>
2019-06-22 23:11:14 -0300

    modificação em comentários da classe Palavras

--- Jogo-Forca/src/jogo/Palavras.java
+++ Jogo-Forca/src/jogo/Palavras.java
@@ -8,17 +8,14 @@ public class Palavras {
      * @author Gabriel
     */
     
-    
     private int indiceSorteado;
-    
     
     public static String [] palavras ={"CAFE","HOMEM","MULHER","MENINIO","MENINA",
         "EU","VOCE","ELE","ELA","CEU","VIOLAO","POSSIVEL","ONIBUS","VEICULO",
         "CASA","TREM","AVIAO","PALAVRA","VIDA","MUSICA","RELOGIO","TEXTO","TELEVISAO",
         "CACHORRO","GATO","MACACO","CHAVE","ESTADO","PAIS","AGUA","SUCO","REFRIGERANTE",
         "COMIDA","CELULAR","PORCO","DENTE","BOCA","ESCOLA","COMPUTADOR","PIANO"};
-//vetor destinada ao armazenamento das palavras
-	
+
 	
 	
     public static String [] dicas = {"BEBIDA", "PESSOA", "PESSOA", "CRIANCA", "CRIANCA",
@@ -28,14 +25,14 @@ public class Palavras {
         "USA NO CARRO", "PORTA", "POSSUI CIDADE", "LOCAL", "MOLHADA", "LIQUIDO",
         "LIQUIDO GASOSO", "ALIMENTO", "ELETRONICO", "SUJO", "BOCA", "DENTISTA", "ESTUDOS",
         "INFORMATICA", "INSTRUMENTO"};
-	//vetor destinado ao armazenamento das dicas
 
-        
+    
     public String setPalavra (){
         Random random = new Random();
         indiceSorteado = random.nextInt(palavras.length);
         return palavras[indiceSorteado];
     }
+    
     
     public String setPalavraUnderline (String palavra){
         String palavraUnderline = "";
@@ -45,7 +42,8 @@ public class Palavras {
         }
         return palavraUnderline;
     }
-     
+    
+    
     public String setDica(){
         return this.dicas[indiceSorteado];
     }
